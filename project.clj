9c9c6226087fc1b6f3092fdebd8716aefc54d1c9
Dan Boykis <danboykis@gmail.com>
2022-09-21 12:32:27 -0400

    update deps

--- project.clj
+++ project.clj
@@ -1,17 +1,10 @@
-(defproject puppetlabs/structured-logging "0.2.1-SNAPSHOT"
+(defproject com.danboykis/structured-logging "0.2.1-SNAPSHOT"
   :description "Write data structures to your logs from clojure."
-  :url "https://github.com/puppetlabs/structured-logging"
+  :url "https://github.com/danboykis/structured-logging"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.logstash.logback/logstash-logback-encoder "4.6"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [cheshire "5.6.3"]
-                 [org.slf4j/slf4j-api "1.7.21"]]
-  :plugins [[lein-release "1.0.5"]]
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
-                                     :username :env/clojars_jenkins_username
-                                     :password :env/clojars_jenkins_password
-                                     :sign-releases false}]]
-  :lein-release {:scm :git
-                 :deploy-via :lein-deploy})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [net.logstash.logback/logstash-logback-encoder "7.2"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [cheshire "5.11.0"]
+                 [org.slf4j/slf4j-api "1.7.36"]])
